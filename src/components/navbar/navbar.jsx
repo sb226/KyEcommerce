557ccfae2a6a49f24082b0c80dd78a00033a44c6
unknown <bachleson@gmail.com>
2025-03-24 15:55:29 +1300

    Add navigation bar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import './navbar.css'
+import logo from '../assets/logo_placeholder.png'
+import cart from '../assets/shopping-cart-line.svg'
+
+function Navbar() {
+    return <div className="navbar">
+        <div className="nav-logo">
+            <a href="#">
+                <img src={logo} alt="" />
+            </a>
+        </div>
+        <ul className="nav-links">
+            <li className="link"><a href="#">Home</a></li>
+            <li id="featureLink" className="link"><a href="#">Features</a></li>
+            <li id="priceLink" className="link"><a href="#">Pricing</a></li>
+            <li id="aboutLink" className="link"><a href="#">About</a></li>
+        </ul>
+        <div className="nav-login-cart">
+            <button className="btn">Login</button>
+            <a href="#">
+                <img src={cart} alt="" />
+            </a>
+        </div>
+    </div>
+}
+
+export default Navbar
