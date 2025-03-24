148b59c318207c3cb1e99e600058dd531bbea075
unknown <bachleson@gmail.com>
2025-03-25 10:03:07 +1300

    Change navbar login button to icon and add cart count to cart

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,28 +1,43 @@
-import React from "react"
-import './Navbar.css'
-import logo from '../assets/logo_placeholder.png'
-import cart from '../assets/shopping-cart-line.svg'
+import React from "react";
+import "./Navbar.css";
+import logo from "../assets/logo_placeholder.png";
+import cart from "../assets/shopping-cart-line.svg";
+import user from "../assets/user-line.svg";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return <div className="navbar">
-        <div className="nav-logo">
-            <a href="#">
-                <img src={logo} alt="" />
-            </a>
-        </div>
-        <ul className="nav-links">
-            <li className="link"><a href="#">Home</a></li>
-            <li id="featureLink" className="link"><a href="#">Features</a></li>
-            <li id="priceLink" className="link"><a href="#">Pricing</a></li>
-            <li id="aboutLink" className="link"><a href="#">About</a></li>
-        </ul>
-        <div className="nav-login-cart">
-            <button className="btn">Login</button>
-            <a href="#">
-                <img src={cart} alt="" />
-            </a>
-        </div>
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <a href="#">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+      <ul className="nav-links">
+        <li className="link">
+          <Link to="/">Home</Link>
+        </li>
+        <li id="featureLink" className="link">
+          <a href="#">Features</a>
+        </li>
+        <li id="priceLink" className="link">
+          <a href="#">Pricing</a>
+        </li>
+        <li id="aboutLink" className="link">
+          <a href="#">About</a>
+        </li>
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login">
+          <img src={user} alt="" />
+        </Link>
+        <Link to="/cart" className="nav-cart">
+          <img src={cart} alt="" />
+          <div className="nav-cart-count">0</div>
+        </Link>
+      </div>
     </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
