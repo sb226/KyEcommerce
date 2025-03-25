7edef72aa20d38771401b626d5afe9e1d2ef136f
unknown <bachleson@gmail.com>
2025-03-25 19:01:40 +1300

    Add routes

---
+++ src/pages/LoginSignup.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LoginSignup() {
+  return <div>This is a log in page</div>;
+}
+
+export default LoginSignup;
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/hero/Hero";
+
+function Shop() {
+  return (
+    <div>
+      <Hero />
+      This is a shopping page
+    </div>
+  );
+}
+
+export default Shop;
