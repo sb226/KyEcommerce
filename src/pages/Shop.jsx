545ae64e69811c13e286b3f4e76ba2bb809cf12a
unknown <bachleson@gmail.com>
2025-03-27 15:53:12 +1300

    Add Offers component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,13 @@
 import Hero from "../components/hero/Hero";
+import Offers from "../components/offers/Offers";
+import Popular from "../components/popular/Popular";
 
 export default function Shop() {
   return (
     <div>
       <Hero />
-      This is a shopping page
+      <Popular />
+      <Offers />
     </div>
   );
 }
