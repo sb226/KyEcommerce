30594e0dc3e24cd66b5b906c06bcca5de53c9e61
unknown <bachleson@gmail.com>
2025-03-27 14:29:45 +1300

    Add Popular component

---
+++ src/components/popular/Popular.jsx
@@ -0,0 +1,44 @@
+import Item from "../item/Item";
+import "./Popular.css";
+import { useEffect, useState } from "react";
+import { fetchProducts } from "../assets/data";
+
+export default function Popular() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProduct() {
+      setLoading(true);
+      const data = await fetchProducts();
+      setProducts(data);
+      setLoading(false);
+    }
+
+    getProduct();
+  }, []);
+
+  if (loading) {
+    return <div>Loading data... Please wait.</div>;
+  }
+
+  return (
+    <div className="popular-container">
+      <h1>POPULAR PRODUCTS</h1>
+      <hr />
+      <div className="popular-item">
+        {products.slice(0, 4).map((item, index) => {
+          return (
+            <Item
+              key={index}
+              thumbnail={item.thumbnail}
+              title={item.title}
+              discountPercentage={item.discountPercentage}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
