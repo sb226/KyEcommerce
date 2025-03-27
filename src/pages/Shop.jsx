279e8bbe8aee7a6ed05789b589e5eab92b45cdf8
unknown <bachleson@gmail.com>
2025-03-27 20:50:42 +1300

    Add Footer component

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,6 +3,7 @@ import NewCollection from "../components/newCollection/NewCollection";
 import Offers from "../components/offers/Offers";
 import Popular from "../components/popular/Popular";
 import NewsLetter from "../components/newsLetter/NewsLetter";
+import Footer from "../components/footer/Footer";
 
 export default function Shop() {
   return (
@@ -12,6 +13,7 @@ export default function Shop() {
       <Offers />
       <NewCollection />
       <NewsLetter />
+      <Footer />
     </div>
   );
 }
