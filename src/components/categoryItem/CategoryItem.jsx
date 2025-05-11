a36380297de9f38694026ef7d1c108b5ff980917
unknown <bachleson@gmail.com>
2025-05-11 19:58:26 +1200

    Add Link so that user can redirect to category when they click on the image

--- src/components/categoryItem/CategoryItem.jsx
+++ src/components/categoryItem/CategoryItem.jsx
@@ -1,17 +1,20 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./CategoryItem.css";
 
 export default function CategoryItem(props) {
   const navigate = useNavigate();
   const { slug, thumbnail, title } = props;
+  const categoryPath = `/category/${slug}`;
 
   function handleCategoryButton() {
-    navigate(`/category/${slug}`);
+    navigate(categoryPath);
   }
 
   return (
     <div className="category-item-container">
-      <img src={thumbnail} alt="" />
+      <Link to={categoryPath}>
+        <img src={thumbnail} alt="" />
+      </Link>
       <div className="category-item">
         <p>{title}</p>
         <button className="category-item-button" onClick={handleCategoryButton}>
