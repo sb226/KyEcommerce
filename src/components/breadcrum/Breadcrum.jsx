9b057905a5578a708326b91f042ebabba651bf9e
unknown <bachleson@gmail.com>
2025-05-12 20:35:45 +1200

    Add react icon to Breadcrum component as well

--- src/components/breadcrum/Breadcrum.jsx
+++ src/components/breadcrum/Breadcrum.jsx
@@ -1,10 +1,12 @@
 import "./Breadcrum.css";
+import { FaChevronRight } from "react-icons/fa";
 
 export default function Breadcrum(props) {
   const { product } = props;
   return (
     <div className="breadcrum">
-      Home &gt; Shop &gt; {product.category} &gt; {product.title}
+      Home <FaChevronRight size={10} /> Shop <FaChevronRight size={10} />{" "}
+      {product.category} <FaChevronRight size={10} /> {product.title}
     </div>
   );
 }
