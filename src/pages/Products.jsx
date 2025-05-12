7328082e753ace31a58a30036696cc1fd4470c5a
unknown <bachleson@gmail.com>
2025-05-12 21:50:40 +1200

    Add function to handle if user type product into the url directly

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,12 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrum from "../components/breadcrum/Breadcrum";
+import { fetchProducts } from "../components/assets/data";
 
 export default function Product() {
   const { products } = useContext(ShopContext);
   const { productId } = useParams();
-  const product = products.find((i) => i.id === Number(productId));
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      setProduct(products.find((i) => i.id === Number(productId)));
+    } else {
+      async function getProduct() {
+        const data = await fetchProducts();
+        setProduct(data.find((i) => i.id === Number(productId)));
+      }
+      getProduct();
+    }
+  }, [products, productId]);
+
+  if (!product) {
+    return <></>;
+  }
+
   return (
     <div>
       <Breadcrum product={product}></Breadcrum>
