968e54d2235764a102aacc6cdcc1c8f61cda6312
unknown <bachleson@gmail.com>
2025-05-16 11:32:20 +1200

    Make offer componet redirect to the right product, also add key for rating star

--- src/components/offers/Offers.jsx
+++ src/components/offers/Offers.jsx
@@ -1,25 +1,33 @@
 import "./Offers.css";
 import { fetchProducts } from "../../api/data";
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Offers() {
   const [image, setImage] = useState("");
+  const [productId, setProductId] = useState("");
+  const navigate = useNavigate();
 
   async function getProduct() {
     const data = await fetchProducts(true);
     setImage(data.images[0]);
+    setProductId(data.id);
   }
 
   useEffect(() => {
     getProduct();
   }, []);
 
+  function handleCheckOutButton() {
+    navigate(`/product/${productId}`);
+  }
+
   return (
     <div className="offers-container">
       <div className="offers-left">
         <h1>Up to 5% OFF</h1>
         <p>What a save !!!</p>
-        <button>Check Now</button>
+        <button onClick={handleCheckOutButton}>Check Now</button>
       </div>
       <div className="offers-right">
         <img src={image} alt="" />
--- src/components/ratingStar/RatingStar.jsx
+++ src/components/ratingStar/RatingStar.jsx
@@ -5,16 +5,16 @@ export default function RatingStar({ rating, size = 25 }) {
   const maxStar = 5;
   return (
     <div className="star-rating-container">
-      {[...Array(Math.floor(rating))].map((_) => (
-        <IoIosStar size={size} />
+      {[...Array(Math.floor(rating))].map((_, index) => (
+        <IoIosStar size={size} key={index} />
       ))}
       {rating % 1 >= 0.5 ? (
         <IoIosStarHalf size={size} />
       ) : (
         <IoIosStarOutline size={size} />
       )}
-      {[...Array(Math.floor(maxStar - rating))].map((_) => (
-        <IoIosStarOutline size={size} />
+      {[...Array(Math.floor(maxStar - rating))].map((_, index) => (
+        <IoIosStarOutline size={size} key={index} />
       ))}
     </div>
   );
